18306019a9b7fe213c5ed1209303aaa3d6161d9b
SMHRD <SMHRD@192.168.219.44>
2025-01-02 10:13:58 +0900

    마이페이지완성

--- SmartBoot1/src/main/java/com/smhrd/basic/controller/MainController.java
+++ SmartBoot1/src/main/java/com/smhrd/basic/controller/MainController.java
@@ -25,73 +25,77 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
-	
-	@Autowired
-	MemberRepo repo;
-	
-	
-	// 회원가입
-	@PostMapping("member/join.do")
-	public String join(MemberVO vo) {
-		MemberEntity en = new MemberEntity(vo);
-		repo.save(en);
-		return "redirect:/";
-	}
-	// 로그인
-	@PostMapping("member/login.do")
-	public String login(String id, String pw, HttpSession session) {
-		MemberEntity enti = repo.findByIdAndPw(id, pw);
-		session.setAttribute("member", enti);
-		return "redirect:/";
-	}
-	// 로그아웃
-	@GetMapping("/logout")
-	public String logout(HttpSession session) {
-		session.removeAttribute("member");
-		return "redirect:/";
-	}
-	
-    
 
-	// 인기도서
-	@Autowired
-	BookRepo bookRepo;
-	// 추천도서
-	@Autowired
-	RecomRepo recomRepo;
-	// 지점 조회
-	@Autowired
-	LibraryRepo libraryRepo;
-	//
-	@Autowired
-	RetentionRepo retentionRepo;
-	
-	@GetMapping("/")
-    public String getBooks(Model model) {
-		
-		// 인기도서
-		List<BookEntity> bestBooks = bookRepo.findByBestSeller("y");
-        model.addAttribute("bestBooks", bestBooks);
+   @Autowired
+   MemberRepo repo;
 
-        // 지역 선택
-        List<String> library = libraryRepo.findRegions();
-		model.addAttribute("library1", library);
-		
-        return "main";
-    }
-	
-	// 지역을 기준으로 도서관 반환
-	@GetMapping("/getLibrariesByRegion")
-	@ResponseBody
-	public List<LibraryEntity> getLibrariesByRegion(@RequestParam String regionNm) {
-	    return libraryRepo.findByRegionNm(regionNm); 
-	}
-	
-	// 도서관 ID로 해당 도서관의 보유 도서 목록 반환 
-	@GetMapping("/library/{libIdx}/books")
-	@ResponseBody
-    public List<BookEntity> getBooksByLibraryId(@PathVariable Integer libIdx) {
-        return bookRepo.findBooksByLibraryId(libIdx);
-    }
+   @GetMapping("/mypage")
+   public String mypage() {
+      return "mypage";
+   }
+
+   // 회원가입
+   @PostMapping("member/join.do")
+   public String join(MemberVO vo) {
+      MemberEntity en = new MemberEntity(vo);
+      repo.save(en);
+      return "redirect:/";
+   }
+
+   // 로그인
+   @PostMapping("member/login.do")
+   public String login(String id, String pw, HttpSession session) {
+      MemberEntity enti = repo.findByIdAndPw(id, pw);
+      session.setAttribute("member", enti);
+      return "redirect:/";
+   }
+
+   // 로그아웃
+   @GetMapping("/logout")
+   public String logout(HttpSession session) {
+      session.removeAttribute("member");
+      return "redirect:/";
+   }
+
+   // 인기도서
+   @Autowired
+   BookRepo bookRepo;
+   // 추천도서
+   @Autowired
+   RecomRepo recomRepo;
+   // 지점 조회
+   @Autowired
+   LibraryRepo libraryRepo;
+   //
+   @Autowired
+   RetentionRepo retentionRepo;
+
+   @GetMapping("/")
+   public String getBooks(Model model) {
+
+      // 인기도서
+      List<BookEntity> bestBooks = bookRepo.findByBestSeller("y");
+      model.addAttribute("bestBooks", bestBooks);
+
+      // 지역 선택
+      List<String> library = libraryRepo.findRegions();
+      model.addAttribute("library1", library);
+
+      return "main";
+   }
+
+   // 지역을 기준으로 도서관 반환
+   @GetMapping("/getLibrariesByRegion")
+   @ResponseBody
+   public List<LibraryEntity> getLibrariesByRegion(@RequestParam String regionNm) {
+      return libraryRepo.findByRegionNm(regionNm);
+   }
+
+   // 도서관 ID로 해당 도서관의 보유 도서 목록 반환
+   @GetMapping("/library/{libIdx}/books")
+   @ResponseBody
+   public List<BookEntity> getBooksByLibraryId(@PathVariable Integer libIdx) {
+      return bookRepo.findBooksByLibraryId(libIdx);
+   }
 
 }
